9345ed37291592d494a5462a3c7f42255cd23cd4
AlphaLabo_Left\meiteiFox <syuumei1119@icloud.com>
2024-12-25 20:59:13 +0900

    Initial

---
+++ Assets/meiteiFox/Cell.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class Cell
+{
+    public struct CellInfo
+    {
+        /// <summary>
+        /// セルのワールド座標
+        /// </summary>
+        public Vector3 Pos { get; set; }
+        /// <summary>
+        /// 0 : 敵のルートではない 1~ : スタートから順にゴールまでの敵が進む順番
+        /// </summary>
+        public int RouteNum { get; set; }
+    }
+    /// <summary>
+    /// 初期化。9*9のセルを生成し、敵のルートをランダムに生成する。
+    /// </summary>
+    /// <param name="leftup">マップの左上</param>
+    /// <param name="rightdown">マップの右下</param>
+    public void Initialize(out CellInfo[] cellInfo,Vector3 topLeft, Vector3 bottomRight)
+    {
+        int gridSize = 9; // 9マス
+        cellInfo = new CellInfo[gridSize * gridSize];
+        float xStep = (bottomRight.x - topLeft.x) / (gridSize - 1);
+        float yStep = (bottomRight.y - topLeft.y) / (gridSize - 1);
+        for (int i = 0;i < gridSize;i++)
+        {
+            for (int j = 0;j < gridSize; i++)
+            {
+                cellInfo[i * gridSize + j] = new CellInfo();
+                cellInfo[i * gridSize + j].Pos = new Vector3(topLeft.x + j * xStep, topLeft.y + i + yStep);
+                cellInfo[i * gridSize + j].RouteNum = 0;
+            }
+        }
+    }
+    /// <summary>
+    /// ルートをランダムに生成する wip
+    /// </summary>
+    void RouteGen(out CellInfo[] cellInfo)
+    {
+        int gridSize = 9;
+        cellInfo = new CellInfo[gridSize * gridSize];
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; i++)
+            {
+                cellInfo[i * gridSize + j].RouteNum = 0;
+            }
+        }
+        //適当に設定 wip
+        cellInfo[1].RouteNum = 1;
+        cellInfo[10].RouteNum = 2;
+        cellInfo[19].RouteNum = 3;
+        cellInfo[28].RouteNum = 4;
+        cellInfo[37].RouteNum = 5;
+        cellInfo[38].RouteNum = 6;
+        cellInfo[39].RouteNum = 7;
+        cellInfo[40].RouteNum = 8;
+        cellInfo[31].RouteNum = 9;
+        cellInfo[22].RouteNum = 10;
+        cellInfo[23].RouteNum = 11;
+        cellInfo[24].RouteNum = 12;
+        cellInfo[25].RouteNum = 13;
+        cellInfo[34].RouteNum = 14;
+        cellInfo[43].RouteNum = 15;
+        cellInfo[52].RouteNum = 16;
+        cellInfo[61].RouteNum = 17;
+        cellInfo[70].RouteNum = 18;
+        cellInfo[69].RouteNum = 19;
+        cellInfo[68].RouteNum = 20;
+        cellInfo[67].RouteNum = 21;
+        cellInfo[76].RouteNum = 22;
+    }
+}
