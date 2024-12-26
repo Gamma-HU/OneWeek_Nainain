919c9c30c8e66753962eca0e6a80cb6574777f27
magumaguro <mk.1_upmushroom@icloud.com>
2024-12-27 00:55:02 +0900

    不是君のボリュームのUI使っていい感じ 降ってくるお菓子

--- Assets/Magumaguro/Title/ModeSelect/ModeSelectButton.cs
+++ Assets/Magumaguro/Title/ModeSelect/ModeSelectButton.cs
@@ -15,4 +15,18 @@ public class ModeSelectButton : MonoBehaviour
     {
         
     }
+
+    public void OnNormalButtonClicked()
+    {
+        Debug.Log("Normal Button Clicked");
+        //ノーマルモードに遷移する
+
+    }
+
+    public void OnEndlessButtonClicked()
+    {
+        Debug.Log("Endless Button Clicked");
+        //エンドレスモードに遷移する
+
+    }
 }
