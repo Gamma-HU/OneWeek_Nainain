efe450b10809ac44dfe825933e3bf3bec3965aa9
meiteiFox <syuumei1119@icloud.com>
2024-12-28 23:20:38 +0900

    waveの実装

---
+++ Assets/meiteiFox/Scripts/EnemyWave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyWave
+{
+    public GameManager.EnemyType enemyType;
+    public int level;        // 敵のレベル
+    [Tooltip("直前の敵が出現してからの経過時間")]
+    public int spawnSec;
+    public EnemyWave(GameManager.EnemyType enemyType, int level, int spawnSec)
+    {
+        this.enemyType = enemyType;
+        this.level = level;
+        this.spawnSec = spawnSec;
+    }
+}
