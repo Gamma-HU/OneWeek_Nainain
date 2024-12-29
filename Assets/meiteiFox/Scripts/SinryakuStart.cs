f534c0b1d6e214176ce925ab390be83fca6f0a42
goroh <65319834+goroh3rd@users.noreply.github.com>
2024-12-29 18:44:15 +0900

    侵略開始/侵略中ボタンのテキスト切り替えを実装

--- Assets/meiteiFox/Scripts/SinryakuStart.cs
+++ Assets/meiteiFox/Scripts/SinryakuStart.cs
@@ -1,12 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class SinryakuStart : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] RectTransform textRectTransform;
     public void Sinryaku_Start()
     {
-        gameManager.Sinryaku_Start();
+        if (gameManager.currentPhase == 0)
+        {
+            gameManager.Sinryaku_Start();
+            SwitchText();
+        }
     }
+    public void SwitchText()
+    {
+        textRectTransform.DOLocalMoveX(290f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() =>
+        {
+            textRectTransform.localPosition = new Vector3(-290f, 0f, 0f);
+            if (gameManager.currentPhase == 0)
+            {
+                text.text = "侵略開始！！";
+            }
+            else
+            {
+                text.text = "侵略中……";
+            }
+            textRectTransform.DOLocalMoveX(0f, 0.5f).SetEase(Ease.OutExpo).SetDelay(0.3f).OnComplete(() =>
+            {
+                Debug.Log("Switched Text");
+            });
+        });
+    }
+
 }
