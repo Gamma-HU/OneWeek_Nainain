c7244f1db3a213e1bb1c6cd9135779eab013a7d4
fuze666 <tuaaribonnbe0313@gmail.com>
2024-12-30 05:44:17 +0900

    装飾品のドラッグアンドドロップ処理の大枠をつくった。

--- Assets/fuze/DecorationDragAndDrag/DecorationDragAndDropManager.cs
+++ Assets/fuze/DecorationDragAndDrag/DecorationDragAndDropManager.cs
@@ -8,13 +8,13 @@ public class DecorationDragAndDropManager : MonoBehaviour
 {
     [SerializeField]string dropTargetTagName = "Player";  // ドラッグされているUIが落とされるKurimanjuUI(Clone)に付けられたタグ。
 
-    [SerializeField]string DecorationUIName = "Nainain";  // ドラッグ可能な装飾品のUIの名前。
+    [SerializeField]string DecorationUIName = "DecorationPanel(Clone)";  // ドラッグ可能な装飾品のUIの名前。
 
     [SerializeField]
     GameObject draggingUI; // ドラッグ中のUI要素を表示するGameObject
     Image draggingUIImage;
     
-    Decoration draggingDecoration;  // 掴んでいる、使う予定の装飾品の情報。
+    DecorationUI draggingDecoration;  // 掴んでいる、使う予定の装飾品の情報。
 
     DropPoint dropPoint; //KurimanjuUIについていて、ドロップしたUIの本体のKurimanjuスクリプトを知っている。
 
@@ -24,23 +24,19 @@ public class DecorationDragAndDropManager : MonoBehaviour
 
 
     void DecorationDropedActin(Kurimanju target){  // targetが、アイテムが使われるKurimanjuスクリプト。合成先。
-        Debug.Log("ターゲット:" + target + "デコ:" + draggingDecoration);
+        Debug.Log("ターゲットのレベル:" + target.Status().level + "デコ:" + draggingDecoration.GetDeco().Status().decoName);
         //target.Equip(GameObject deco, int rank) Equipを呼ぶ。
     }
 
     public void OnClickItem(GameObject clickDeco){
-        this.draggingDecoration = clickDeco.GetComponent<Decoration>();
+        this.draggingDecoration = clickDeco.GetComponent<DecorationUI>();  // draggingDecorationに情報を登録
         draggingUIImage.enabled = true;
-        draggingUIImage.sprite = draggingDecoration.sprite;
-        Debug.Log("SetDraggingUI!!");
+        draggingUIImage.sprite = draggingDecoration.GetSprite();  // draggingUIImageに画像を表示
     }
     void Update(){
         if(Input.GetMouseButtonDown(0)){
-            PointerEventData pointData = new PointerEventData(EventSystem.current);
-            pointData.position = Input.mousePosition;
-            List<RaycastResult> RayResult = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointData, RayResult);
-            foreach (RaycastResult result in RayResult){
+                List<RaycastResult> rayResult = RaycastAll();
+                foreach (RaycastResult result in rayResult){
                 var getObj = result.gameObject;
                 if(getObj.name == DecorationUIName){
 
@@ -51,11 +47,8 @@ public class DecorationDragAndDropManager : MonoBehaviour
         }
         if(Input.GetMouseButtonUp(0)){
             if(draggingDecoration != null){
-                PointerEventData pointData = new PointerEventData(EventSystem.current);
-                pointData.position = Input.mousePosition;
-                List<RaycastResult> RayResult = new List<RaycastResult>();
-                EventSystem.current.RaycastAll(pointData, RayResult);
-                foreach (RaycastResult result in RayResult){
+                List<RaycastResult> rayResult = RaycastAll();
+                foreach (RaycastResult result in rayResult){
                     if(result.gameObject.tag == dropTargetTagName){
 
                         Kurimanju target = result.gameObject.GetComponent<DropPoint>().Hontai(); // これこそが装飾品を装備するKurimanjuのスクリプト
@@ -66,6 +59,13 @@ public class DecorationDragAndDropManager : MonoBehaviour
             }
             draggingDecoration = null;
             draggingUIImage.enabled = false;
+        }
+        List<RaycastResult> RaycastAll(){
+            PointerEventData pointData = new PointerEventData(EventSystem.current);
+            pointData.position = Input.mousePosition;
+            List<RaycastResult> rayResult = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointData, rayResult);
+            return rayResult;
         }
     }
 }
--- Assets/fuze/DecorationDragAndDrag/DecorationUI.cs
+++ Assets/fuze/DecorationDragAndDrag/DecorationUI.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DecorationUI : MonoBehaviour // これをドラッグするためのUI要素のプレハブ（DecoPanel）につけて、生成するときに適当なDecorationをセットしてほしい。
+public class DecorationUI : MonoBehaviour // これをドラッグするためのUI要素のプレハブ（DecorationPanel）につけて、生成するときに適当なDecorationとSpriteをセットしてほしい。
 {
     
     Decoration decoration;
+    Sprite sprite;
+
+
     public Decoration GetDeco(){
         return decoration;
     }
     public void SetDeco(Decoration decoration){
         this.decoration = decoration;
+    }
+
+    public Sprite GetSprite(){
+        return sprite;
+    }
+    public void SetSprite(Sprite sprite){
+        this.sprite = sprite;
     }
 
 }
